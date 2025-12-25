da65eab4bd3a94bd60067273eb878e70bb771d7c
FyukMdaa <fyukmdaa@tutanota.com>
2025-12-25 21:32:54 +0900

    change sops-key path

--- nixos/modules/sops.nix
+++ nixos/modules/sops.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  
+  sops.age.keyFile = "~/.config/sops/age/key.txt";
+
   # デフォルトのsecretsファイル
   sops.defaultSopsFile = ../../secrets/ssh.yaml;
-  
+
   sops.secrets.github_ssh_key = {
     owner = config.users.users.fyukmdaa.name;
     group = config.users.users.fyukmdaa.group;
