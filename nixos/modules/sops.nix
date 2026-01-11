fe39a6f2238b163f0a04a26293fb79cc1602e546
FyukMdaa <mdaatiu@outlook.com>
2026-01-11 15:12:08 +0900

    fix token config

--- nixos/modules/sops.nix
+++ nixos/modules/sops.nix
@@ -1,19 +1,18 @@
 { config, pkgs, ... }:
-
 {
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-
-  # デフォルトのsecretsファイル
   sops.defaultSopsFile = ../../secrets/ssh.yaml;
-
+  
   sops.secrets.github_ssh_key = {
     owner = config.users.users.fyukmdaa.name;
     group = config.users.users.fyukmdaa.group;
     mode = "0600";
     path = "/home/fyukmdaa/.ssh/id_ed25519";
   };
- sops.secrets.github_token = {
+  
+  sops.secrets.github_token = {
     owner = config.users.users.fyukmdaa.name;
+    group = config.users.users.fyukmdaa.group;
     mode = "0600";
     path = "/home/fyukmdaa/.config/nix/github-token";
   };
